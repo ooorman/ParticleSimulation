1a0455635d54ca80188a4a3fcf6f1f8ea5d7a708
ooorman <111591292+ooorman@users.noreply.github.com>
2024-05-24 18:12:12 +0300

    Update simulation.py
    оптимизация

--- simulation.py
+++ simulation.py
@@ -1,13 +1,21 @@
 import math
+# import time
+import time
+
+import numpy as np
+from scipy.spatial import KDTree
 import pygame
 from examples import *
 
 pygame.init()
 all_particles = []
+activeHeight = 800
+activeWidth = 800
+
 
 class Particle:
-    def __init__(self, position, power, weight, gravity=0, size=10):
-        self.color = (255, 0, 0) if power == 1 else (0,255,0)
+    def __init__(self, position, power, weight, size=5):
+        self.color = (255, 0, 0) if power == 1 else (0, 255, 0)
         self.size = size
         self.position = position
         self.power = power
@@ -16,71 +24,72 @@ class Particle:
         self.impulse_x = 0
         self.impulse_y = 0
         self.collisions = []
-        self.gravity = gravity
 
-        all_particles.append(self)
-
-    def get_particles_in_action_radius(self, all_particles):
-        self_x, self_y = self.position['x'], self.position['y']
+    def get_particles_in_action_radius(self, tree, all_particles):
+        self_x = self.position['x']
+        self_y = self.position['y']
+        self_power = self.power
         self.collisions.clear()
-        for particle in all_particles:
+        indices = tree.query_ball_point([self_x, self_y], self.action_radius)
+        for idx in indices:
+            particle = all_particles[idx]
             if particle is not self:
-                dx = self_x - particle.position['x']
-                dy = self_y - particle.position['y']
-                dist = math.hypot(dx, dy)
-
-                if dist <= self.action_radius and dist != 0:
+                dx = particle.position['x'] - self_x
+                dy = particle.position['y'] - self_y
+                dist = dx * dx + dy * dy
+                if dist != 0:
                     self.collisions.append(particle)
-
-                    factor = (particle.power * self.power) / dist
-                    self.impulse_y += dy * factor
-                    self.impulse_x += dx * factor
-        self.impulse_y -= self.weight * self.gravity
+                    factor = particle.power * self_power / math.sqrt(dist)
+                    particle.impulse_y += dy * factor
+                    particle.impulse_x += dx * factor
 
     def impulse_to_coords(self):
         self.position['x'] += int(self.impulse_x) / self.weight
         self.position['y'] += int(self.impulse_y) / self.weight
 
+
 def render_particles(screen, particles_list, selection, font, rentgen, is_stop):
     screen.fill((255, 255, 255))
-
     # Отрисовка выделенной области и подсчет частиц внутри нее
     if selection:
         pygame.draw.rect(screen, (0, 0, 180), selection)
+    # Отрисовка линий для rentgen
+    if rentgen:
+        processed_collisions = set()
+
+        for particleA in particles_list:
+            for particleB in particleA.collisions:
+                if particleB not in processed_collisions:
+                    pygame.draw.line(screen, (150, 150, 150), (particleA.position['x'], particleA.position['y']), (particleB.position['x'], particleB.position['y']), 2)
+                    processed_collisions.add(particleA)
+    # Отрисовка всех частиц
+    for particle in particles_list:
+        pygame.draw.circle(screen, particle.color, (particle.position['x'], particle.position['y']), particle.size)
+
+    if selection:
         pr_count = sum(1 for particle in particles_list if selection.collidepoint(particle.position['x'], particle.position['y']) and particle.power == 1)
         el_count = sum(1 for particle in particles_list if selection.collidepoint(particle.position['x'], particle.position['y']) and particle.power != 1)
         sum_energy = round(sum(abs(particle.impulse_x) + abs(particle.impulse_y) for particle in particles_list if selection.collidepoint(particle.position['x'], particle.position['y'])))
 
         text = f"{sum_energy}, -{el_count}, +{pr_count}"
         screen.blit(font.render(text, True, (255, 255, 255)), (selection.bottomright[0] - len(text) * 10, selection.bottomright[1] - 30))
-
-    # Отрисовка линий для rentgen
-    if rentgen:
-        for particleA in particles_list:
-            for particleB in particleA.collisions:
-                dist = math.dist((particleB.position['x'], particleB.position['y']), (particleA.position['x'], particleA.position['y']))
-                if dist <= particleA.action_radius:
-                    pygame.draw.line(screen, (128, 128, 128), (particleA.position['x'], particleA.position['y']), (particleB.position['x'], particleB.position['y']), 3)
-
     # Отображение общей кинетической энергии, если игра остановлена
     if is_stop:
         total_kinetic_energy = round(sum(abs(particle.impulse_y + particle.impulse_x) for particle in particles_list))
         text = f"Общее кол-во кинетической энергии: {total_kinetic_energy}"
-        screen.blit(font.render(text, True, (0, 0, 0)), (800 - len(text) * 11, 800 - 50))
-
-    # Отрисовка всех частиц
-    for particle in particles_list:
-        pygame.draw.circle(screen, particle.color, (particle.position['x'], particle.position['y']), particle.size / 2)
+        screen.blit(font.render(text, True, (0, 0, 0)), (activeWidth - len(text) * 11, activeHeight - 50))
 
     pygame.display.update()
 
+
 def main():
     global all_particles
-    screen = pygame.display.set_mode((800, 800))
+
+    screen = pygame.display.set_mode((activeWidth, activeHeight))
     pygame.display.set_caption('Симуляция')
 
     # #Заполняем массив частицами из тестовых примеров. Можно поменять на test2 и т.д.
-    all_particles += test11()
+    all_particles += test1()
 
     stop = False
     clock = pygame.time.Clock()
@@ -92,6 +101,7 @@ def main():
     rentgen = False
     while True:
 
+        time_start = time.time()
         if not pygame.mouse.get_pressed()[0]:
             selecting = False
         if pygame.key.get_pressed()[pygame.K_UP]:
@@ -101,16 +111,16 @@ def main():
             if FPS - 10 >= 60:
                 FPS -= 10
         if not stop:
-            for particle in all_particles:
-                cond = True
-                if not 1 <= particle.position['x'] + particle.impulse_x / particle.weight <= 799:
-                    particle.impulse_x *= -1
-                if not 1 <= particle.position['y'] + particle.impulse_y / particle.weight <= 799:
-                    particle.impulse_y *= -1
+            tree = KDTree(np.array([[p.position['x'], p.position['y']] for p in all_particles]))
 
-                if cond:
-                    particle.get_particles_in_action_radius(all_particles)
             for particle in all_particles:
+                particle.get_particles_in_action_radius(tree, all_particles)
+
+            for particle in all_particles:
+                if not 0 < particle.position['x'] + particle.impulse_x / particle.weight < activeWidth:
+                    particle.impulse_x *= -1
+                if not 0 < particle.position['y'] + particle.impulse_y / particle.weight < activeHeight:
+                    particle.impulse_y *= -1
                 particle.impulse_to_coords()
 
         for event in pygame.event.get():
@@ -142,8 +152,9 @@ def main():
                     first_selecting_pos = False
                     selection = False
         render_particles(screen, all_particles, selection, font, rentgen, stop)
-
+        print(time.time() - time_start)
         clock.tick(FPS)
+
 
 if __name__ == '__main__':
     main()
